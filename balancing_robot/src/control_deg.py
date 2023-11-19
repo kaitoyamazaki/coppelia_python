7509b1660625727607e03dd1616ebff582d55eea
kaitoyamazaki <reflect018@yahoo.co.jp>
2023-11-19 17:26:33 +0900

    sim.setJointTargetPosition()に関する調査を実行, 弧度法で角度を指定することがわかった

--- balancing_robot/src/control_deg.py
+++ balancing_robot/src/control_deg.py
@@ -11,21 +11,23 @@ def __init__(sim):
 def main():
     client = RemoteAPIClient()
     sim = client.require('sim')
+    target = -45
     motor_r, motor_l, IMU, target_ang = __init__(sim)
-    print(f"test")
     sim.setStepping(True)
     sim.startSimulation()
+    sim.setJointTargetPosition(motor_r, np.deg2rad(target))
+    sim.setJointTargetPosition(motor_l, np.deg2rad(target))
     while (t := sim.getSimulationTime()) < 10:
-        IMU_ang = sim.getObjectOrientation(IMU, sim.handle_world)
-        diff_ang = target_ang - IMU_ang[0]
-        diff_ang = np.rad2deg(diff_ang)
+        #IMU_ang = sim.getObjectOrientation(IMU, sim.handle_world)
+        #diff_ang = target_ang - IMU_ang[0]
+        #diff_ang = np.rad2deg(diff_ang)
         # 振動の様子も見たいので若干数値を加算する
         #diff_ang = diff_ang
-        sim.setJointTargetPosition(motor_r, -diff_ang)
-        sim.setJointTargetPosition(motor_l, -diff_ang)
-        now_target_position_r = sim.getJointTargetPosition(motor_r)
-        now_target_position_l = sim.getJointTargetPosition(motor_l)
-        print(f"currently target angle, right motor : {np.rad2deg(now_target_position_r)}, left motor : {np.rad2deg(now_target_position_l)}")
+        #sim.setJointTargetPosition(motor_r, -diff_ang)
+        #sim.setJointTargetPosition(motor_l, -diff_ang)
+        #now_target_position_r = sim.getJointTargetPosition(motor_r)
+        #now_target_position_l = sim.getJointTargetPosition(motor_l)
+        #print(f"currently target angle, right motor : {np.rad2deg(now_target_position_r)}, left motor : {np.rad2deg(now_target_position_l)}")
         sim.step()
     sim.stopSimulation()
 
