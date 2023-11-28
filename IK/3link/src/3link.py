1fe8fd3c810b686b13635b68a09893554de2e4ae
kaitoyamazaki <reflect018@yahoo.co.jp>
2023-11-28 12:14:12 +0900

    J1の角度のみ表示した

--- IK/3link/src/3link.py
+++ IK/3link/src/3link.py
@@ -27,9 +27,69 @@ class Threelink():
     def simulation(self):
         sim = self.sim
 
+        sim.setStepping(True)
+        sim.startSimulation()
 
+        i = 0.5
 
+        while (t := sim.getSimulationTime()) < 70:
+            #print(f"Simulation time: {t: .2f} [s]")
+            self.move_arm(i)
+            sim.step()
+            i = i + 0.00005
+        
+        sim.stopSimulation()
 
+    def move_arm(self, x):
+        sim = self.sim
+        l1 = self.l1
+        l2 = self.l2
+        l3 = self.l3
+        l4 = self.l4
+
+        j1 = self.j1
+        j2 = self.j2
+        j3 = self.j3
+
+        xe = x
+        ye = 0
+        theta_e = np.deg2rad(-90)
+
+        p3_x = xe - 0.2
+        p3_y = 0
+
+        l5 = p3_x
+
+        #j1の角度を求める
+        c1 = (l2 * l2 + l5 * l5 - l3 * l3) / (2 * l2 * l5)
+        #print(f"c1 is {c1}")
+        j1_rad = np.arccos(c1)
+        j1_rad = j1_rad - (np.pi / 2)
+
+        #j2の角度を求める
+        c2 = (l2 * l2 + l3 * l3 - l5 * l5) / (2 * l2 * l3)
+        #print(f"c2 is {c2}")
+        beta = np.arccos(c2)
+        j2_rad = np.pi - beta
+        j2_rad = -j2_rad
+
+        #j3の角度を求める
+
+        j3_motor = theta_e -j1_rad -j2_rad
+
+        joint = np.empty(3)
+
+        joint[0] = j1_rad
+        joint[1] = j2_rad
+        joint[2] = j3_motor
+
+        #joint_deg = np.rad2deg(joint)
+
+        #print(f"{joint_deg}")
+
+        sim.setJointPosition(j1, j1_rad)
+        sim.setJointPosition(j2, j2_rad)
+        sim.setJointPosition(j3, j3_motor)
 
 
 def main():
