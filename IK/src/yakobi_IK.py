2b74a196c7349351749a2dd0ff87ead88e90661f
kaitoyamazaki <reflect018@yahoo.co.jp>
2023-12-04 19:04:07 +0900

    dpを導出する機能を実装, 次はヤコビ行列を実装する

--- IK/src/yakobi_IK.py
+++ IK/src/yakobi_IK.py
@@ -50,13 +50,24 @@ class Simulation:
 
 
         while (t := sim.getSimulationTime()) < 50:
+
+            # 現在の手先位置を取得
             now_pe_pos = self.get_pe_pos()
+
+            # 現在の位置からの進行方向を決定する
             direction = self.decide_direction(target_pos, now_pe_pos)
 
+            # 次の場所を導出
             next_pe_pos = self.get_next_pe(self.dp, direction, now_pe_pos)
 
-            print(f"direction is {direction}")
+            # ヤコビ行列を適用するための位置に関する偏差を導出
+            currentry_dp = self.get_dp(next_pe_pos, now_pe_pos)
+
+            
+
+            #print(f"direction is {direction}")
             #print(f"next_pe is {next_pe_pos}")
+            #print(f"currentry_dp is {currentry_dp}")
             sim.step()
         
         sim.stopSimulation()
@@ -114,7 +125,7 @@ class Simulation:
         return direction_flg
 
         
-    def get_pe_pos(self): ## エンドエフェクタp_eの座標を二次元で取得する関数
+    def get_pe_pos(self): ## エンドエフェクタp_eの座標を2次元で取得する関数
         
         sim = self.sim
         pe_pos = sim.getObjectPosition(self.p_e, sim.handle_world)
@@ -131,7 +142,7 @@ class Simulation:
         dx = direction[0][0] * dp
         dy = direction[0][1] * dp
 
-        print(f"dx, dy is ({dx}, {dy})")
+        #print(f"dx, dy is ({dx}, {dy})")
 
         next_pos = np.empty((1,2))
         next_pos[0][0] = now_pos[0][0] + dx
@@ -140,78 +151,84 @@ class Simulation:
         return next_pos
 
 
+    def get_dp(self, t_i1, t_i):
 
+        dp = np.empty((1,2))
+        dp[0][0] = t_i1[0][0] - t_i[0][0]
+        dp[0][1] = t_i1[0][1] - t_i[0][1]
+
+        return dp
     
-    def get_div_pos(self, x, y):
-        l1 = self.l1
-        l2 = self.l2
+    #def get_div_pos(self, x, y):
+        #l1 = self.l1
+        #l2 = self.l2
 
-        first_x = l1 * np.cos(0) + l2 * np.cos(np.deg2rad(0+90))
-        first_y = l1 * np.sin(0) + l2 * np.sin(np.deg2rad(0+90))
+        #first_x = l1 * np.cos(0) + l2 * np.cos(np.deg2rad(0+90))
+        #first_y = l1 * np.sin(0) + l2 * np.sin(np.deg2rad(0+90))
 
-        dx = x - first_x
-        dy = y - first_y
+        #dx = x - first_x
+        #dy = y - first_y
 
-        dq = np.empty((2,1))
-        dq[0][0] = dx
-        dq[1][0] = dy
+        #dq = np.empty((2,1))
+        #dq[0][0] = dx
+        #dq[1][0] = dy
 
-        print(f"{dq}")
+        #print(f"{dq}")
 
-        return dq
+        #return dq
 
 
-    def input_target(self): #目標値を設定する関数, 最悪変数はグローバル化でも良いかも
+    #def input_target(self): #目標値を設定する関数, 最悪変数はグローバル化でも良いかも
 
-        target_pos = np.empty((1,2))
+        #target_pos = np.empty((1,2))
 
-        target_pos[0][0] = self.target_pos_x
-        target_pos[0][1] = self.target_pos_y
+        #target_pos[0][0] = self.target_pos_x
+        #target_pos[0][1] = self.target_pos_y
 
-        #x, y = map(float, input("Enter x, y of the Endeffector : ").split(','))
+        ##x, y = map(float, input("Enter x, y of the Endeffector : ").split(','))
 
-        return target_pos
+        #return target_pos
 
 
 
     # ここからは使わない予定の関数, 今後削除の予定
 
-    def IK(self, j1, j2, dq):
-        l1 = self.l1
-        l2 = self.l2
+    #def IK(self, j1, j2, dq):
+        #l1 = self.l1
+        #l2 = self.l2
 
-        now_rad = self.get_now_joint()
+        #now_rad = self.get_now_joint()
 
-        first_IK = self.calc_IK(l1, l2, j1, j2)
+        #first_IK = self.calc_IK(l1, l2, j1, j2)
 
-        result = np.dot(first_IK, dq)
+        #result = np.dot(first_IK, dq)
 
-        result = now_rad + result
+        #result = now_rad + result
 
-        print(f"result is {np.rad2deg(result)}")
+        #print(f"result is {np.rad2deg(result)}")
 
-        return result
+        #return result
 
 
 
     
-    def get_now_joint(self):
-        now_joint = np.empty((2,1))
-        now_joint[0][0] = self.first_deg_joint1
-        now_joint[1][0] = self.first_deg_joint2
+    #def get_now_joint(self):
+        #now_joint = np.empty((2,1))
+        #now_joint[0][0] = self.first_deg_joint1
+        #now_joint[1][0] = self.first_deg_joint2
 
-        print(f"{now_joint}")
+        #print(f"{now_joint}")
 
-        return now_joint
+        #return now_joint
     
-    def calc_IK(self, l1, l2, j1, j2):
-        IK_queue = np.empty((2,2))
-        IK_queue[0][0] = (l2 * np.cos(j1 + j2)) / (l1 * l2 * np.sin(j2))
-        IK_queue[0][1] = (l2 * np.sin(j1 + j2)) / (l1 * l2 * np.sin(j2))
-        IK_queue[1][0] = (-l1 * np.cos(j1) - l2 * np.cos(j1 + j2)) / (l1 * l2 * np.sin(j2))
-        IK_queue[1][1] = (-l1 * np.sin(j1) - l2 * np.sin(j1 + j2)) / l1 * l2 * np.sin(j2)
+    #def calc_IK(self, l1, l2, j1, j2):
+        #IK_queue = np.empty((2,2))
+        #IK_queue[0][0] = (l2 * np.cos(j1 + j2)) / (l1 * l2 * np.sin(j2))
+        #IK_queue[0][1] = (l2 * np.sin(j1 + j2)) / (l1 * l2 * np.sin(j2))
+        #IK_queue[1][0] = (-l1 * np.cos(j1) - l2 * np.cos(j1 + j2)) / (l1 * l2 * np.sin(j2))
+        #IK_queue[1][1] = (-l1 * np.sin(j1) - l2 * np.sin(j1 + j2)) / l1 * l2 * np.sin(j2)
 
-        return IK_queue
+        #return IK_queue
 
     
 def main():
