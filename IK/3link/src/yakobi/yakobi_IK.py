713e20e870931391b69919bf0b0217fdff6c3cb1
kaitoyamazaki <reflect018@yahoo.co.jp>
2023-12-13 13:14:44 +0900

    初期位置と進行方向を決定する関数を実装

--- IK/3link/src/yakobi/yakobi_IK.py
+++ IK/3link/src/yakobi/yakobi_IK.py
@@ -22,6 +22,7 @@ class Simulation:
 
         self.target_pos_x = 1.3
         self.target_pos_z = 0.4
+        self.target_pos_theta = 90
         self.dp = 0.001
 
         self.p_target = sim.getObject("/p_target")
@@ -45,8 +46,9 @@ class Simulation:
         # 最終的な目標値の入力を行う
         target_pos = self.input_target_pos()
 
-        # 目標位置表示
+        # 目標位置と目標姿勢を表示
         sim.setObjectPosition(self.p_target, [target_pos[0][0], 0.0, target_pos[0][1]], sim.handle_world)
+        sim.setObjectOrientation(self.p_target, [0.0, np.deg2rad(target_pos[0][2]), 0.0], sim.handle_world)
     
         while (t := sim.getSimulationTime()) < 10:
 
@@ -59,29 +61,33 @@ class Simulation:
             # 次の時間周期でのpeの位置を導出する
             next_pe_pos = self.get_next_pe_pos(self.dp, direction, now_pe_pos)
 
-            print(f"{next_pe_pos}")
+            print(f"{direction}")
 
 
             sim.step()
         
         sim.stopSimulation()
 
+    # 目標値を設定する関数
     def input_target_pos(self):
     
-        target_pos = np.empty((1,2))
+        target_pos = np.empty((1,3))
 
         target_pos[0][0] = self.target_pos_x
         target_pos[0][1] = self.target_pos_z
+        target_pos[0][2] = np.deg2rad(self.target_pos_theta)
 
         return target_pos
     
+    # 手先の位置と姿勢を取得する関数
     def get_pe_pos_xz(self):
 
         sim = self.sim
         pos = sim.getObjectPosition(self.p_e, sim.handle_world)
-        pos = [pos[0], pos[2]]
+        ori = sim.getObjectOrientation(self.p_e, sim.handle_world)
+        pos = [pos[0], pos[2], ori[1]]
 
-        now_pe_pos = np.empty((1,2))
+        now_pe_pos = np.empty((1,3))
         for i in range(len(pos)):
             now_pe_pos[0][i] = pos[i]
 
@@ -90,7 +96,7 @@ class Simulation:
     def decide_direction(self, now_pos, target_pos):
 
         sim = self.sim
-        direction_flg = np.empty(2)
+        direction_flg = np.empty(3)
 
         for i in range(len(direction_flg)):
 
