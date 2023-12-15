59eef6ff8569d7ec94758deda7dcb2587b93d5e0
kaitoyamazaki <reflect018@yahoo.co.jp>
2023-12-15 11:45:37 +0900

    位置・姿勢を格納する

--- IK/3link/src/yakobi/first_yakobi.py
+++ IK/3link/src/yakobi/first_yakobi.py
@@ -32,6 +32,7 @@ class Simulation:
         self.l2 = 0.5
         self.l3 = 0.5
     
+    # シミュレーションを実施する関数
     def simulation(self):
 
         sim = self.sim
@@ -41,9 +42,19 @@ class Simulation:
 
         pos_of_p_t = self.assign_p_t_pos()
 
-        print(pos_of_p_t)
+        # 目標位置と姿勢を表示
 
-    
+        sim.setObjectPosition(self.p_t, [pos_of_p_t[0][0], pos_of_p_t[0][1], 0.05], sim.handle_world)
+        sim.setObjectOrientation(self.p_t, [np.deg2rad(self.target_pos_theta), 0.0, 0.0], sim.handle_world)
+
+        while (t := sim.getSimulationTime()) < 50:
+
+            # 現在の手先に関する位置情報を取得
+            now_pe_pos = self.get_pe_pos_xy()
+
+            print(f"{now_pe_pos}")
+
+    # 目標位置・姿勢を代入する関数
     def assign_p_t_pos(self):
         
         pos_of_p_t = np.empty((1,3))
@@ -53,6 +64,24 @@ class Simulation:
         pos_of_p_t[0][2] = self.target_pos_theta
 
         return pos_of_p_t
+    
+    def get_pe_pos_xy(self):
+
+        sim = self.sim
+        
+        p_e_pos = sim.getObjectPosition(self.p_e, sim.handle_world)
+        p_e_ori = sim.getObjectOrientation(self.p_e, sim.handle_world)
+
+        pos = [p_e_pos[0], p_e_pos[1], p_e_ori[0]]
+
+        p_e_posture = np.empty((1,3))
+        
+        for i in range(len(pos)):
+
+            p_e_posture[0][i] = pos[i]
+        
+        return p_e_posture
+
 
 
 def main():
