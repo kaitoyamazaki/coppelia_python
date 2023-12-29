6ccf11bf1c5a5add5832fe7ef0dd248457db9f4b
kaitoyamazaki <reflect018@yahoo.co.jp>
2023-12-29 18:33:49 +0900

    台形制御でヤコビ行列を用いた制御モドキの完成?

---
+++ velocity_control/coppelia/yakobi.py
@@ -0,0 +1,130 @@
+from coppeliasim_zmqremoteapi_client import RemoteAPIClient
+import numpy as np
+import pandas as pd
+import csv
+import time
+
+class Simulation:
+
+    def __init__(self):
+        
+        self.client = RemoteAPIClient()
+        client = self.client
+        self.sim = client.require('sim')
+        sim = self.sim
+
+        self.j1 = sim.getObject("/BaseRobot/j1")
+        self.j2 = sim.getObject("/BaseRobot/j2")
+        self.j3 = sim.getObject("/BaseRobot/j3")
+        self.j4 = sim.getObject("/BaseRobot/j4")
+        self.j5 = sim.getObject("/BaseRobot/j5")
+        self.j6 = sim.getObject("/BaseRobot/j6")
+        self.cop = sim.getObject("/BaseRobot/CoP")
+
+        self.l1 = 0.13156
+        self.l2 = 0.1104
+        self.l3 = 0.096
+        self.l4 = 0.07318
+
+        self.filepath = "../data/trapezoidal_data.csv"
+        self.df = pd.read_csv(self.filepath, header=None)
+        self.data = self.df.values
+
+
+        
+    def simulation(self):
+        
+        sim = self.sim
+        data = self.data
+
+        sim.setStepping(True)
+        sim.startSimulation()
+
+        for i in range(len(data)):
+
+            theta2 = sim.getJointPosition(self.j2)
+            theta3 = sim.getJointPosition(self.j3)
+            theta4 = sim.getJointPosition(self.j4)
+
+            yakobi = self.calc_yakobi_row(theta2, theta3, theta4)
+            yakobi_inv = np.linalg.inv(yakobi)
+
+            theta = np.empty((1,3))
+            theta[0][0] = theta2
+            theta[0][1] = theta3
+            theta[0][2] = theta4
+            theta = theta.T
+            dp = self.calc_dp(data[i])
+
+            new_theta = theta + np.dot(yakobi_inv, dp)
+
+            sim.setJointPosition(self.j2, new_theta[0][0])
+            sim.setJointPosition(self.j3, new_theta[1][0])
+            sim.setJointPosition(self.j4, new_theta[2][0])
+
+
+            #print(f"{theta2}, {theta3}, {theta4}")
+            #print(f"{yakobi}")
+            #print(f"{yakobi_inv}")
+            print(f"{dp}")
+            print(f"{new_theta}")
+
+
+            sim.step()
+            time.sleep(0.1)
+        
+        sim.stopSimulation()
+    
+    def calc_yakobi_row(self, j2, j3, j4):
+
+        l2 = self.l2
+        l3 = self.l3
+        l4 = self.l4
+
+        yakobi = np.empty((3,3))
+
+        yakobi[0][0] = -l2 * np.sin(j2) - l3 * np.sin(j2+j3) - l4 * np.sin(j2+j3+j4)
+        yakobi[0][1] = -l3 * np.sin(j2+j3) - l4 * np.sin(j2+j3+j4)
+        yakobi[0][2] = -l4 * np.sin(j2+j3+j4)
+
+        yakobi[1][0] = l2 * np.cos(j2) + l3 * np.cos(j2+j3) + np.cos(j2+j3+j4)
+        yakobi[1][1] = l3 * np.cos(j2+j3) + np.cos(j2+j3+j4)
+        yakobi[1][2] = np.cos(j2+j3+j4)
+
+        yakobi[2][0] = 1
+        yakobi[2][1] = 1
+        yakobi[2][2] = 1
+
+        return yakobi
+    
+    def calc_dp(self, data):
+
+        sim = self.sim
+
+        print(f"{data}")
+        pos = sim.getObjectPosition(self.cop, sim.handle_world)
+        y_dp = 0
+        z_dp = data[2] - pos[2]
+        theta_dp = -0.03
+        dp = np.empty((3,1))
+        dp[0][0] = y_dp
+        dp[1][0] = z_dp
+        dp[2][0] = theta_dp
+
+        return dp
+
+
+
+def main():
+    try:
+        simulation = Simulation()
+        simulation.simulation()
+    except KeyboardInterrupt:
+        print(f"Ctrl-Cによる終了")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print(f"Ctrl-Cによる終了")
