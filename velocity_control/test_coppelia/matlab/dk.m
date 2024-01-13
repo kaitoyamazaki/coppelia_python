72cd7eb0fa615fc7e578ae836a06489f97b6b701
kaitoyamazaki <reflect018@yahoo.co.jp>
2024-01-13 18:54:16 +0900

    matlabでダイレクトキネマティクスを実行

---
+++ velocity_control/test_coppelia/matlab/dk.m
@@ -0,0 +1,9 @@
+joint_deg = [20 -30 -60];
+joint = deg2rad(joint_deg);
+link = [0 0.4 0.2];
+
+p = [-(link(2)*cos(joint(1))*sin(joint(2)) + link(3)*cos(joint(1))*sin(joint(2)+joint(3)));
+     -(link(2)*sin(joint(1))*sin(joint(2)) + link(3)*sin(joint(1))*sin(joint(2)+joint(3)));
+     link(1) + link(2)*cos(joint(2)) + link(3)*cos(joint(2) + joint(3))];
+
+disp(p);
