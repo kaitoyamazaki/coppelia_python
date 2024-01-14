e11d4c09587f856df9fd82b9a20f7cd58c5fac33
kaitoyamazaki <reflect018@yahoo.co.jp>
2024-01-14 10:39:41 +0900

    台形制御のプログラムが完成, このあとに進行方向の要素を導入する

--- velocity_control/test_coppelia/src/making_path.py
+++ velocity_control/test_coppelia/src/making_path.py
@@ -37,7 +37,29 @@ class MakingPath:
         print(f"t, velocity, position")
 
         for t in np.arange(0.0, self.t_f+0.1, 0.1):
-            print({t})
+
+            if(t <= 0.1):
+                velocity2 = 0
+                velocity3 = 0
+                velocity1 = acceleation * t
+                position1 = velocity1 * t / 2
+
+            elif(t > 0.1 and t <=5.0):
+                velocity1 = 0
+                velocity3 = 0
+                velocity2 = self.target_veloccity
+                position2 = velocity2 * (t - 0.1)
+            
+            elif(t > 5.0):
+                velocity1 = 0
+                velocity2 = 0
+                velocity3 = -acceleation * (t - 5.0) + self.target_veloccity
+                position3 = (self.target_veloccity + velocity3) * (t - 5.0) / 2
+
+            velocity = velocity1 + velocity2 + velocity3
+            position = position1 + position2 + position3
+
+            print(f"{t}, {velocity}, {position}")
 
 
 def main():
