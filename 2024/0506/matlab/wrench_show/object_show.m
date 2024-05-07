cd675cdff8a2eda4dc75d9b94ece15216b7960b6
kaitoyamazaki <reflect018@yahoo.co.jp>
2024-05-07 18:45:09 +0900

    typeB時の法線ベクトルが変化するものを作成

--- 2024/0506/matlab/wrench_show/object_show.m
+++ 2024/0506/matlab/wrench_show/object_show.m
@@ -7,6 +7,9 @@ y_c1 = 27.5;
 x_c2 = -7.5;
 y_c2 = 15.0;
 
+x_cop = (x_c1 + x_c2) / 2;
+y_cop = (y_c1 + y_c2) / 2;
+
 r = 2.5;
 r2 = 1;
 r3 = 1.5;
@@ -37,6 +40,9 @@ y_cir21 = r2 * sin(theta2) + y_c2;
 cog_x = r3 * cos(theta);
 cog_y = r3 * sin(theta);
 
+Cop_x = r3 * cos(theta) + x_cop;
+Cop_y = r3 * sin(theta) + y_cop;
+
 
 % 図形のプロット
 figure;
@@ -51,6 +57,7 @@ fill(x_cir2, y_cir2, [0.7, 1, 0.7]);
 fill(x_cir11, y_cir11, [0.0, 0.0, 0.0]);
 fill(x_cir21, y_cir21, [0.0, 0.0, 0.0]);
 fill(cog_x, cog_y, [0.0, 0.0, 0.0]);
+fill(Cop_x, Cop_y, [0.0, 0.0, 0.0]);
 
 axis equal; % 各軸のスケールを等しく保持
 
@@ -64,6 +71,7 @@ load('data/typeB_coeffs.mat');
 
 rows = size(coeff_num);
 
+pause(5);
 
 for i=1:rows(1);
     edit_alpha = alpha;
@@ -73,15 +81,22 @@ for i=1:rows(1);
     edit_alpha = edit_alpha * coeff_num(i, 1);
     edit_beta = edit_beta * coeff_num(i, 2);
     edit_gamma = edit_gamma * coeff_num(i, 3);
+
+    edit_cop_x = edit_alpha;
+    edit_cop_y = edit_beta + edit_gamma;
     %coeffs = [edit_alpha, edit_beta, edit_gamma];
     %disp(coeffs);
     h1 = quiver(7.5, 30, 0.0, edit_alpha, 'Color', 'k', 'LineWidth', 2.0, 'AutoScale', 'off');
     h2 = quiver(5.0, 27.5, edit_beta, 0.0, 'Color', 'k', 'LineWidth', 2.0, 'AutoScale', 'off');
     h3 = quiver(-5.0, 15.0, edit_gamma, 0.0, 'Color', 'k', 'LineWidth', 2.0, 'AutoScale', 'off');
-    pause(0.20);
+    h4 = quiver(x_cop, y_cop, edit_cop_x, edit_cop_y, 'Color', 'r', 'LineWidth', 3.0, 'AutoScale', 'off');
+    h5 = quiver(0.0, 0.0, -edit_cop_x, -edit_cop_y, 'Color', 'r', 'LineWidth', 3.0, 'AutoScale', 'off');
+    pause(0.30);
     delete(h1);
     delete(h2);
     delete(h3);
+    delete(h4);
+    delete(h5);
 end
 
 hold off;
--- 2024/0506/matlab/wrench_show/random_wrench_vector_plot.m
+++ 2024/0506/matlab/wrench_show/random_wrench_vector_plot.m
@@ -2,17 +2,17 @@
 
 clear;
 
-load('data/random_points_in_wrench_typeB.mat');
+load('data/random_points_typeB.mat');
 openfig(['data/typeB_wrench.fig']);
 
 hold on;
 
-rows = size(random_points_in_wrench_typeB, 1);
+rows = size(random_points_typeB, 1);
 pause(5);
 
 for i = 1:1:rows
-    %h = quiver3(0, 0, 0, random_points_in_wrench_typeBs_in_wrench_typeB(i, 1), random_points_in_wrench_typeB(i, 2), random_points_in_wrench_typeB(i, 3), 'LineWidth', 1.5);
-    h = quiver3(0, 0, 0, random_points_in_wrench_typeB(i, 1), random_points_in_wrench_typeB(i, 2), random_points_in_wrench_typeB(i, 3), 'Color', 'k', 'LineWidth', 1.5);
+    %h = quiver3(0, 0, 0, random_points_typeBs_in_wrench_typeB(i, 1), random_points_typeB(i, 2), random_points_typeB(i, 3), 'LineWidth', 1.5);
+    h = quiver3(0, 0, 0, random_points_typeB(i, 1), random_points_typeB(i, 2), random_points_typeB(i, 3), 'Color', 'k', 'LineWidth', 1.5);
     pause(0.20);
     delete(h);
 end
